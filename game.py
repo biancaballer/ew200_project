7339a08a4db0b0f62b496bf0d201d358c9b9c2e0
biancaballer <m261212@usna.edu>
2023-11-13 09:18:20 -0500

    settings added

--- game.py
+++ game.py
@@ -1,3 +1,11 @@
 import pygame
+import sys
+import random
+from settings import *
+pygame.init()
 
-print("hello world!")
+game_font = pygame.font.Font("assets/fonts/spaceage.ttf", 128)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Meteor Blast")
+# projectiles - use trig for x and y speed
---
+++ settings.py
@@ -0,0 +1,4 @@
+# DIMENSIONS
+TILE_SIZE = 64  # tiles are square height == width
+SCREEN_WIDTH = 12 * TILE_SIZE
+SCREEN_HEIGHT = 8 * TILE_SIZE
