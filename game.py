1abbd48c7f6dd0e82b0fd2f302e53de6e0366304
biancaballer <m261212@usna.edu>
2023-11-16 09:32:25 -0500

    stars blit on screen and shuttle moves

--- game.py
+++ game.py
@@ -7,20 +7,18 @@ from settings import *
 
 pygame.init()
 
-game_font = pygame.font.Font("assets/fonts/spaceage.ttf", 128)
+game_font = pygame.font.Font(None, 50) # none works
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Meteor Blast")
 # make space stuff
-sand_top = pygame.image.load("assets/images/sand_top.png").convert()
-seagrass = pygame.image.load("assets/images/seagrass.png").convert()
-seagrass.set_colorkey((0, 0, 0))
-sand_top.set_colorkey((0, 0, 0))
+star = pygame.image.load("assets/images/star1.png").convert()
+star.set_colorkey((0, 0, 0))
 score = 0
-my_fish = fish.Fish(200, 200)  # create a new fish
+my_shuttle = shuttle.Shuttle(20, 380)  # create a new shuttle
 
-for _ in range(NUM_MINNOWS):
-    minnows.add(Minnow(random.randint(0, SCREEN_WIDTH - TILE_SIZE),
+for _ in range(NUM_METEORS):
+    meteors.add(Meteor(random.randint(0, SCREEN_WIDTH - TILE_SIZE),
                        random.randint(0, SPACE_BOTTOM - TILE_SIZE)))
 
 background = screen.copy()
@@ -30,17 +28,13 @@ clock = pygame.time.Clock()
 def draw_background():
     # draw space
     background.fill(SPACE_COLOR)
-    # sandy bottom
-    for i in range(SCREEN_WIDTH // TILE_SIZE):
-        background.blit(sand, (TILE_SIZE * i, SCREEN_HEIGHT - TILE_SIZE))
-        background.blit(sand_top, (TILE_SIZE * i, SCREEN_HEIGHT - (2 * TILE_SIZE)))
     # randomly place stars
-    for _ in range(4):
+    for _ in range(25):
         x = random.randint(0, SCREEN_WIDTH)
-        # offset the seaweed so it looks better :)
-        y = random.randint(SCREEN_HEIGHT - 2 * TILE_SIZE, SCREEN_HEIGHT) - (0.5 * TILE_SIZE)
-        background.blit(seagrass, (x, y))
-    # draw the CHOMP! title
+        # offset the stars so it looks better :)
+        y = random.randint(0, SCREEN_HEIGHT) - (0.5 * TILE_SIZE) # stars go all over screen
+        background.blit(star, (x, y))
+    # draw the meteor blast title
     text = game_font.render("Meteor Blast", True, (216, 242, 242))
 
     background.blit(text, (SCREEN_WIDTH // 2 - text.get_width() // 2, SCREEN_HEIGHT // 2))
@@ -48,7 +42,7 @@ def draw_background():
 
 draw_background()
 
-while len(minnows)>0:
+while len(meteors) > 0:
     # listen for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -56,40 +50,31 @@ while len(minnows)>0:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                my_fish.moving_left = True
+                my_shuttle.moving_left = True
             if event.key == pygame.K_RIGHT:
-                my_fish.moving_right = True
-            if event.key == pygame.K_UP:
-                my_fish.moving_up = True
-            if event.key == pygame.K_DOWN:
-                my_fish.moving_down = True
+                my_shuttle.moving_right = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
-                my_fish.moving_left = False
+                my_shuttle.moving_left = False
             if event.key == pygame.K_RIGHT:
-                my_fish.moving_right = False
-            if event.key == pygame.K_UP:
-                my_fish.moving_up = False
-            if event.key == pygame.K_DOWN:
-                my_fish.moving_down = False
+                my_shuttle.moving_right = False
         # elif event.type == pygame.MOUSEMOTION:
-            # my_minnow.rect.center = pygame.mouse.get_pos()
-
+        # my_minnow.rect.center = pygame.mouse.get_pos()
 
     # update game objects
-    my_fish.update()
-    minnows.update()
+    my_shuttle.update()
+    meteors.update()
 
     # check for collisions
-    chomped_minnows = pygame.sprite.spritecollide(my_fish, minnows, True)
-    score += len(chomped_minnows)
-    if len(chomped_minnows) >0:
-        print(f"Chomped a minnow, your score is {score}!")
+    blasted_meteors = pygame.sprite.spritecollide(my_shuttle, meteors, True)
+    score += len(blasted_meteors)
+    if len(blasted_meteors) > 0:
+        print(f"You blasted a meteor, your score is {score}!")
     # draw the game screen
     screen.blit(background, (0, 0))
-    my_fish.draw(screen)
+    my_shuttle.draw(screen)
 
-    minnows.draw(screen)
+    meteors.draw(screen)
     pygame.display.flip()
     clock.tick(60)
 # projectiles - use trig for x and y speed
