12a588ff4166c52e0b6d798c77f983970eee220d
biancaballer <m261212@usna.edu>
2023-12-02 20:49:33 -0500

    laser blast sound added and end/restart screen almost fully functional

--- game.py
+++ game.py
@@ -17,7 +17,6 @@ pygame.display.set_caption("Meteor Blast")
 # make space stuff
 star = pygame.image.load("assets/images/star1.png").convert()
 star.set_colorkey((0, 0, 0))
-score = 0
 my_shuttle = shuttle.Shuttle(20, 380)  # create a new shuttle
 my_planet = planet.Planet(500, 30)
 scaled_planet = pygame.transform.scale(my_planet.image, (int(my_planet.rect.width * SCALE_FACTOR),
@@ -26,6 +25,7 @@ my_planet.image = scaled_planet
 pygame.mixer.music.load("assets/sounds/space_music.mp3")
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1)  # plays background music in infinite loop
+blaster_sound = pygame.mixer.Sound("assets/sounds/laserblast.mp3")
 for _ in range(NUM_METEORS):
     meteors.add(Meteor(random.randint(0, 10),
                        random.randint(0, 0)))
@@ -50,58 +50,101 @@ def draw_background():
     background.blit(text, (SCREEN_WIDTH // 2 - text.get_width() // 2, SCREEN_HEIGHT // 2))
 
 
-draw_background()
-game_over = False  # credit Arya
-hits = 0
-# main game loop
-while not game_over:
-    # listen for events
-    # continuously adds a new meteor periodically
-    if random.randint(1, 50) == 1:
-        new_meteor = Meteor(80, 20)
-        meteors.add(new_meteor)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                my_shuttle.moving_left = True
-            if event.key == pygame.K_RIGHT:
-                my_shuttle.moving_right = True
-            if event.key == pygame.K_SPACE:
-                my_shuttle.shoot()
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                my_shuttle.moving_left = False
-            if event.key == pygame.K_RIGHT:
-                my_shuttle.moving_right = False
-
-    # update game objects
-    my_shuttle.update()
-    meteors.update()
-
-    # check for collisions
-
-    blasted_meteors = pygame.sprite.groupcollide(meteors, lasers, True, True)
-    score += len(blasted_meteors)
-    if len(blasted_meteors) > 0:
-        print(f"You blasted a meteor, your score is {score}!")
-    # draw the game screen
-    screen.blit(background, (0, 0))
-    my_shuttle.draw(screen)
-    my_planet.draw(screen)
-    lasers.draw(screen)
-    lasers.update()
-
-    hit_planet = pygame.sprite.spritecollide(my_planet, meteors, False)
-    if hit_planet:
-        hits += 1
-        # my_planet.hit()  # changes planet image to show damage
-    if hits >= 3:
-        game_over = True
-
-    meteors.draw(screen)
+# Function to display end screen
+def end_screen():
+    screen.fill(SPACE_COLOR)
+    text = game_font.render("Game Over - Press R to Restart or Q to Quit", True, 0)
+    text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+    screen.blit(text, text_rect)
     pygame.display.flip()
-    clock.tick(60)
-# projectiles - use trig for x and y speed
+
+    waiting_for_key = True
+    while waiting_for_key:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    main_game()
+                    return True
+                elif event.key == pygame.K_q:
+
+                    pygame.quit()
+                    sys.exit()
+
+    return False
+
+
+# main game loop
+def main_game():
+    game_over = False  # credit Arya
+    music_paused = False  # flag for background music
+    score = 0
+    hits = 0
+    while not game_over:
+        # listen for events
+        # continuously adds a new meteor periodically
+        if random.randint(1, 50) == 1:
+            new_meteor = Meteor(80, 20)
+            meteors.add(new_meteor)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    my_shuttle.moving_left = True
+                if event.key == pygame.K_RIGHT:
+                    my_shuttle.moving_right = True
+                if event.key == pygame.K_SPACE:
+                    my_shuttle.shoot()
+                    if not music_paused:
+                        # Pause the background music
+                        pygame.mixer.music.pause()
+                        music_paused = True
+                        # Play the additional sound effect
+                        blaster_sound.play()
+                    else:
+                        # Unpause the background music
+                        pygame.mixer.music.unpause()
+                        music_paused = False
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    my_shuttle.moving_left = False
+                if event.key == pygame.K_RIGHT:
+                    my_shuttle.moving_right = False
+
+        # update game objects
+        my_shuttle.update()
+        meteors.update()
+
+        # check for collisions
+
+        blasted_meteors = pygame.sprite.groupcollide(meteors, lasers, True, True)
+        score += len(blasted_meteors)
+        if len(blasted_meteors) > 0:
+            print(f"You blasted a meteor, your score is {score}!")
+        # draw the game screen
+        screen.blit(background, (0, 0))
+        my_shuttle.draw(screen)
+        my_planet.draw(screen)
+        lasers.draw(screen)
+        lasers.update()
+
+        hit_planet = pygame.sprite.spritecollide(my_planet, meteors, False)
+        if hit_planet:
+            hits += 1
+            # my_planet.hit()  # changes planet image to show damage
+        if hits >= 3:
+            # game_over = True
+            end_screen()
+
+        meteors.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+
+
+draw_background()
+main_game()
