7708eabfdcd722c07c9d1794550d26bed65216e9
biancaballer <m261212@usna.edu>
2023-12-03 21:11:42 -0500

    trig added - game done

--- README.md
+++ README.md
@@ -4,7 +4,18 @@
 Save the planet by blasting any meteors heading its way!
 
 My game incorporates:
-list of points from rubric
+keyboard king (key press events)
+sound blaster (sound effects based on game events)
+points-r-us (track player score)
+over achiever (track high score between game plays)
+physical (some type of pysics - gravity in space)
+shooter (projectiles)
+helping hand
+shifting screens (multiple screens)
+fancy fonts (online font)
+the walls are hard (collisions constraint player movement)
+muzak (continuous music)
+great graphics (change sprite based on dynamics)
 
 Special Thanks to:
 Arya Singh, Pach
--- laser.py
+++ laser.py
@@ -1,18 +1,28 @@
 import pygame
 from settings import *
+import math
 
 
 class Laser(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
+        # Get the current mouse position
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        # Calculate the angle between the mouse position and the center of the shuttle
+        angle = math.atan2(mouse_y, mouse_x)
+
         self.right_image = pygame.image.load("assets/images/laser.png").convert()
         self.rect = self.right_image.get_rect()
         self.right_image.set_colorkey((0, 0, 0))
         self.image = self.right_image
         self.rect = pygame.rect.Rect(x, y, self.image.get_width(), self.image.get_height())
+        self.angle = math.radians(90)  # Set the angle to 90 degrees
 
     def update(self):
-        self.rect.y -= 5
+        laser_speed = 5
+        self.rect.y -= laser_speed * math.sin(self.angle)
+        # self.rect.y -= 5
         if self.rect.y > SCREEN_HEIGHT:
             self.kill()
 
